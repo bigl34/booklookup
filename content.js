b9078d83fd184e88d196977c6a5ef8b2755fdc57
bigl34 <120096872+bigl34@users.noreply.github.com>
2022-12-08 16:44:27 +0000

    Update content.js

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 let s = document.createElement('script');
 
-s.src = chrome.runtime.getURL('support/goodreads/script.js');
+s.src = chrome.runtime.getURL('script.js');
 
 document.head.appendChild(s);
